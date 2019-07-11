1dec40f11d68ddeb8b9821ce0dcf724bb6680188
Vladimir Boldovskyy <vboldovskyy@gmail.com>
2019-07-11 17:41:30 +0300

    corrected formatting in main class

--- src/com/cursor/comparators/Main.java
+++ src/com/cursor/comparators/Main.java
@@ -1,9 +1,8 @@
 package com.cursor.comparators;
 
 public class Main {
-
     public static void main(String[] args) {
-      new CustomComparatorTest().checkAll();
+        new CustomComparatorTest().checkAll();
     }
-    }
+}
 
